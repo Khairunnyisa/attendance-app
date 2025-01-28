4988901ca27244eb9bde3a4886e45fd083b89c3a
“Khairunnyisa” <khairunnyisa@gmail.com>
2025-01-28 08:53:56 +0700

    finishing timestamp config

--- lib/services/timestamp_service.dart
+++ lib/services/timestamp_service.dart
@@ -1,5 +1,6 @@
 import 'package:intl/intl.dart';
 
+// mendapatkan data secara realtime, memanggil kelas yg disediakan dari librari intl
 void setDateTime(Function(String, String, String) onDateTimeSet) {
   var dateNow = DateTime.now();
   var dateFormat = DateFormat('dd mm yyyy');
@@ -10,4 +11,22 @@ void setDateTime(Function(String, String, String) onDateTimeSet) {
   String currentDate = '$date | $time';
 
   onDateTimeSet(date, time, currentDate);
+}
+
+void setAttendanceStatus(Function(String) onStatusSet) {
+  var dateNow = DateTime.now();
+  // int.parse = mengubah integer jadi string
+  var hour = int.parse(DateFormat('hh').format(dateNow));
+  var minute = int.parse(DateFormat('mm').format(dateNow));
+  // peng kondisian timestamp
+  String attendanceStatus;
+  if (hour <= 7 || (hour == 7 && minute <= 00)) {
+    attendanceStatus = "Attend";
+  } else if (hour > 7 || (hour == 7 && minute <= 01)) {
+    attendanceStatus = "Late";
+  } else {
+    attendanceStatus = "Absent";
+  }
+
+  onStatusSet(attendanceStatus);
 }
