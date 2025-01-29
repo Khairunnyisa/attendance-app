10081d12765f1ad271c377aa02eab0d9e0a2d0fb
“Khairunnyisa” <khairunnyisa@gmail.com>
2025-01-29 11:16:25 +0700

    creating a submit button component -- TBC

--- lib/ui/attend/components/location.dart
+++ lib/ui/attend/components/location.dart
@@ -23,8 +23,22 @@ Padding buildLocationSection(bool isLoading, String address) {
             : Padding(
                 padding: EdgeInsets.all(10),
                 child: SizedBox(
+                  // agar tingginya itu proporsi menjadi 120 pixel (mengatur tinggi dari textfield)
                   height: 5 * 24,
-                  child: TextField(),
+                  child: TextField(
+                    enabled: false,
+                    maxLines: 5,
+                    decoration: InputDecoration(
+                        alignLabelWithHint: true,
+                        disabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(color: Colors.blueAccent)),
+                        hintText:
+                            address.isNotEmpty ? address : "your  location",
+                        hintStyle: TextStyle(fontSize: 14, color: Colors.grey),
+                        fillColor: Colors.transparent,
+                        filled: true),
+                  ),
                 ),
               )
       ],
