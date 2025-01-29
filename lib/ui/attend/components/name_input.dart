2bf3e8623731e4e7d7b5a2979e9ea2cdbdc9407c
“Khairunnyisa” <khairunnyisa@gmail.com>
2025-01-29 10:43:54 +0700

    make an name input controller

---
+++ lib/ui/attend/components/name_input.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+Padding buildNameInputField(TextEditingController controllerName) {
+  return Padding(
+    padding: EdgeInsets.all(10),
+    child: TextField(
+      textInputAction: TextInputAction.done,
+      keyboardType: TextInputType.text,
+      controller: controllerName,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.symmetric(horizontal: 10),
+          labelText: "Your Name",
+          labelStyle: TextStyle(fontSize: 14, color: Colors.blueAccent),
+          hintText: "please enter your name",
+          hintStyle: TextStyle(
+            fontSize: 14,
+            color: Colors.grey,
+          ),
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(color: Colors.blueAccent)),
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(color: Colors.blueAccent))),
+    ),
+  );
+}
