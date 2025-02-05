8289f49cd04f4d70c9718f657a39e2dabbb9309a
“Khairunnyisa” <khairunnyisa@gmail.com>
2025-02-05 09:34:30 +0700

    finishing face_detector function

--- lib/ui/attend/camera-component/face_detector.dart
+++ lib/ui/attend/camera-component/face_detector.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:attendance_app/utils/google_ml_kit.dart';
 import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
 import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
 
 class FaceDetectorComponent{
@@ -13,5 +16,42 @@ class FaceDetectorComponent{
   Future<void> detectFaces(XFile image) async {
     final inputImage = InputImage.fromFilePath(image.path);
     final faces = await faceDetector.processImage(inputImage);
+
+    // kaalu misalkan face nya terdeteksi
+    for (Face face in faces) {
+      // boundingbox: untuk ngambil component dari gambar
+      // Rect: agar proses absensi tidak bisa ditipu
+      final Rect boundingBox = face.boundingBox;
+
+      // untuk menghandle posisi wajah secara vertical
+      final double? verticalPosition = face.headEulerAngleY;
+
+      // untuk menghandle posisi wajah secara horizontal
+      final double? horizontalProsition = face.headEulerAngleZ;
+
+      // perkondisian apabile face landmark sudah aktif, ditantai oleh (mulut, mata, pipi, dan hidung, telinga)
+      final FaceLandmark? leftEar = face.landmarks[FaceLandmarkType.leftEar];
+      // kalau telinga kiri nya terdeteksi, dia akan mereturn posisi telinga tersebut, yg dikembalikan adalah titik koordinat nya
+      if (leftEar != null ) {
+        final Point<int> leftEarPosition = leftEar.position;
+        print("Left Ear Position: $leftEarPosition");
+        
+      }
+
+      // perkondisian apabila wajahnya terdeteksi (ditandai dengan bibir tersenyum)
+      if (face.smilingProbability != null) {
+        final double? smileProbability = face.smilingProbability;
+        print("Smile Probability: $smileProbability");
+        
+      }
+      // perkondisian apabila fitur tracking wajah sudah aktif
+      if (face.trackingId != null) {
+        final int? trackingId = face.trackingId;
+        print('Tracking ID $trackingId');
+        
+      }
+      finshing face_detector function
+      
+    }
   }
 }
